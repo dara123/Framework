d8fd28d8694dd4c3e9d559c8ee1c41861b387e42
dara <dara@ilabsinc.com>
2016-08-10 21:32:20 -0400

    add container

---
+++ Xuexue/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Xuexue;
+/**
+ * Description of ServiceProvider
+ *
+ * @author DARA123
+ */
+class ServiceProvider 
+{
+    //put your code here
+}
